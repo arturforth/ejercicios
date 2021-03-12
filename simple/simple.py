5bd39d9317afe5b232e5a764cf679b6f2f91113a
metalfanbase <43826270+metalfanbase@users.noreply.github.com>
2021-03-12 10:13:30 -0300

    Simple v1

--- simple/simple.py
+++ simple/simple.py
@@ -6,10 +6,33 @@ def gendir(longitud=10):
     res = []
 
     for i in range(longitud):
-        res.append({i:rd.randint(1, 100)})
+        res.append({'id':i,'edad':rd.randint(1, 100)})
 
     return res
 
 
+def sortdir(lista):
+
+    c = 0
+
+    while 1:
+        for i in range(len(lista)-1):
+            if lista[i]['edad'] < lista[i+1]['edad']:
+                a = lista[i]
+                b = lista[i+1]
+                lista[i] = b
+                lista[i+1] = a
+                c += 1
+        if c > 0:
+            c = 0
+        else:
+            break
+
+    return lista
+
+
 rv = gendir()
+
+rv_sorted = sortdir(rv)
+
 print('Fin')
